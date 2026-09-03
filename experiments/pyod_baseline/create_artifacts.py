3469c144e0acf1a7fba4c63acbfc206f3b109c4d
Anagha Dhekne <anaghagdhekne7@gmail.com>
2026-09-03 18:59:56 +0000

    style: fix experiment import ordering

--- experiments/pyod_baseline/create_artifacts.py
+++ experiments/pyod_baseline/create_artifacts.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
+from pyod.utils.persistence import save as pyod_save
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 
 import modelstamp
-from pyod.utils.persistence import save as pyod_save
 
 
 OUT = Path("experiments/pyod_baseline/artifacts")
